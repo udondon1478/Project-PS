50ea745ec16fd2b21a4da35a077309df3644abda
udondon1478 <udon1478don@gmail.com>
2025-05-10 11:18:02 +0900

    Revert "feat: Google と Discord の OAuth プロバイダー設定を追加 :rocket:"
    This reverts commit adda992afd6da85f0e61081df687f0d451a55aa7.

--- src/auth.ts
+++ src/auth.ts
@@ -2,17 +2,11 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { PrismaClient } from "@prisma/client";
  
-import Discord from "next-auth/providers/discord";
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    Discord({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
   ],
   callbacks: {
