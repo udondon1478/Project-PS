b479faad61bd8ef17acd94d933f71f1dc55c5e3d
udondon1478 <udon1478don@gmail.com>
2025-05-15 13:30:59 +0900

    style: トップページでヘッダーや検索バーにページ上部の要素が重なってしまう問題を、余白を設けることで対処 :nail_care:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 pt-40">
       <h1 className="text-2xl font-bold mb-6">最新の商品</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
