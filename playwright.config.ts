529abfd3bf07368e705a989fe91861e99d32810e
udondon1478 <udon1478don@gmail.com>
2025-11-18 20:39:24 +0900

    chore: 重複していたworkersとretriesの宣言を削除

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,8 +17,6 @@ const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 
 export default defineConfig({
   fullyParallel: false,
-  workers: 1,
-  retries: 2,
 
   // テストファイルの場所
   testDir: './e2e',
