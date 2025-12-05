40e16bb39f978c8bcf560cdf738d6b9a46f0a666
udondon1478 <udon1478don@gmail.com>
2025-12-05 18:32:33 +0900

    fix: i18n初期化時のフォールバック言語とエラーハンドリングを追加 :rotating_light:

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -17,12 +17,16 @@ i18n
   .init({
     resources,
     lng: 'ja', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    fallbackLng: 'ja',
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
+  })
+  .catch((error) => {
+    console.error('i18n initialization failed:', error);
   });
 
 export default i18n;
