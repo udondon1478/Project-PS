42aafbd710bf0e7ba50945c7deaff1494ac33503
udondon1478 <udon1478don@gmail.com>
2025-12-07 16:30:44 +0900

    style: フッターに左右の余白(px-4)を追加 :nail_care:

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { ExternalLink } from 'lucide-react';
 export default function Footer() {
   return (
     <footer className="border-t bg-background py-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+      <div className="container flex flex-col items-center justify-between gap-4 px-4 md:h-24 md:flex-row">
         <div className="flex flex-col items-center md:items-start order-2 md:order-1">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             © 2024 PolySeek
