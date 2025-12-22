6669d2e98bf9eb31be8bcb984aa39e7d27aaedf3
udondon1478 <udon1478don@gmail.com>
2025-12-22 12:34:59 +0900

    style: サービス紹介を最上部に移動し見出しを更新 :nail_care:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,8 +38,8 @@ export default function Home() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-6">最新の商品</h1>
       <ServiceIntroSection />
+      <h1 className="text-2xl font-bold mb-6">データベースに登録された最新の商品</h1>
       <ProductGrid 
         products={products} 
         showLikeButton={true} 
