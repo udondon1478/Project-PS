f49b1a5b15c8ea18d4d057e1a968ec10164519c5
udondon1478 <udon1478don@gmail.com>
2026-01-12 14:44:31 +0900

    chore: ecosystem.config.jsをcjsにリネーム :wrench:

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,10 @@ module.exports = {
     {
       name: 'booth-cron',
       script: 'src/scripts/booth-cron.ts',
-      interpreter: 'node_modules/.bin/ts-node', // Use ts-node to run TypeScript directly
+      interpreter: 'node_modules/.bin/tsx',
+      interpreter_args: '--tsconfig tsconfig.json',
       cwd: './',
+      exec_mode: 'fork', // Required for custom interpreter
       instances: 1,
       autorestart: true,
       watch: false,
